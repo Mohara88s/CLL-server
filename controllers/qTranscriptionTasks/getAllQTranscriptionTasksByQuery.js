82ae0507b261de003dab6aaa31ca8b73cfa75edc
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2022-04-19 12:55:59 +0300

    fix all tasks response

--- controllers/qTranscriptionTasks/getAllQTranscriptionTasksByQuery.js
+++ controllers/qTranscriptionTasks/getAllQTranscriptionTasksByQuery.js
@@ -4,13 +4,11 @@ const getAllQTranscriptionTasksByQuery = async (req, res, next) => {
   const { page = 1, limit = 20, query = "" } = req.query;
   const skip = (page - 1) * limit;
   const querySt = { eng: { $regex: `.*${query}.*` } };
-  const result = await Task.find(querySt)
+  const tasks = await Task.find(querySt)
     .skip(skip)
     .limit(+limit);
   res.status(200).json({
-    data: {
-      result,
-    },
+      tasks,
   });
 };
 
