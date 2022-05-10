516d4e97acdac6f20411d9ab42f466159262b94c
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2022-05-10 16:51:14 +0300

    add populate owner to get dictionary

--- controllers/own-dictionaries/getOwnDictionaryById.js
+++ controllers/own-dictionaries/getOwnDictionaryById.js
@@ -4,10 +4,15 @@ const { NotFound } = require("http-errors");
 const getOwnDictionaryById = async (req, res) => {
 	const { dictionaryId } = req.params;
 
-	const ownDictionary = await OwnDictionary.findById(dictionaryId).populate({
-		path: "ownDictionaryTasks",
-		select: ["eng", "rus", "utrn", "qtrn"],
-	});
+	const ownDictionary = await OwnDictionary.findById(dictionaryId)
+		.populate({
+			path: "ownDictionaryTasks",
+			select: ["eng", "rus", "utrn", "qtrn"],
+		})
+		.populate({
+			path: "ownDictionaryOwner",
+			select: "name",
+		});
 
 	if (!ownDictionary) {
 		throw new NotFound(`Own dictionary with id=${dictionaryId} not found`);
