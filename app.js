aa3d155e916946540e344c73c0810425e8028c08
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2022-07-21 16:15:54 +0300

    fix swagger server

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const options = {
       version: '1.0.0',
       description: 'Express Library API'
     },
-    servers: [{ url: 'http://localhost:3000' }]
+    // servers: [{ url: 'http://localhost:3000' }]
   },
   apis: ['./routes/api/*.js']
 }
