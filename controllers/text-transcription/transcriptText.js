b158ea7c96b8b46ca5eb2b045a408f02613506ae
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2023-10-20 08:05:15 +0300

    fixed transcription with punctuation

--- controllers/text-transcription/transcriptText.js
+++ controllers/text-transcription/transcriptText.js
@@ -1,6 +1,9 @@
 const { TranscriptionTask: Task } = require("../../models");
 const { BadRequest } = require("http-errors");
-
+const { createIntlSegmenterPolyfill } = require('intl-segmenter-polyfill')
+const fs = require('fs')
+const wasmBuffer = fs.readFileSync('node_modules/intl-segmenter-polyfill/dist/break_iterator.wasm')
+let wasmBinary = new Uint8Array(wasmBuffer)
 const transcriptionTypes = ['U-transcription', 'Q-transcription'];
 
 const transcriptText = async (req, res) => {
@@ -8,10 +11,10 @@ const transcriptText = async (req, res) => {
 	if (!englishText) {
 		throw new BadRequest("Text is required");
 	}
-	if (!transcriptionType) { 
+	if (!transcriptionType) {
 		throw new BadRequest("Transcription type is required");
 	}
-		const transcriptionAdres = {};
+	const transcriptionAdres = {};
 	switch (transcriptionType) {
 		case transcriptionTypes[0]:
 			transcriptionAdres.adres = 'utrn'
@@ -21,23 +24,30 @@ const transcriptText = async (req, res) => {
 			break;
 		default:
 			throw new BadRequest("Transcription type not found");
-	  }
-	const textArray = englishText.match(/\b(\w+)\b/g)
+	}
+	// const textArray = englishText
+	// .match(/\b(\w+)\b/g)
+	// .match(/[\p{L}\p{N}\p{S}\p{M}]+/gu)
+
+	const Segmenter = await createIntlSegmenterPolyfill(wasmBinary);
+	const segmenter = new Segmenter("en", { granularity: 'word' });
+	const textArray = segmenter.segment(englishText);
 
 	const transcriptedTextArray =
 		await Promise.all(textArray.map(async (e) => {
-			const query = {
-				$or: [
-					{ eng: e },
-					{ eng: e.toLowerCase() },
-				]
-			}
-			const task = await Task.find(query)
-			
-			if (task.length === 0) { return '#' + e } else { return task[0][transcriptionAdres.adres].split(',')[0] }
-		}))
+			if (e.isWordLike) {
+				const query = {
+					$or: [
+						{ eng: e.segment },
+						{ eng: e.segment.toLowerCase() },
+					]
+				}
+				const task = await Task.find(query)
 
-	const text = transcriptedTextArray.join(" ")
+				if (task.length === 0) { return { ...e, segment: '#' + e.segment } } else { return { ...e, segment: task[0][transcriptionAdres.adres].split(',')[0] } }
+			} else return e
+		}))
+	const text = transcriptedTextArray.map(e => { return e.segment }).join('')
 
 	res.status(200).json({
 		text,
