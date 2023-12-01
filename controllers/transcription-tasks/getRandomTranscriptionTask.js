a6a00528362ece246fd98ef9e89b9b52b4b1750c
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2023-12-01 09:45:01 +0200

    made faster getRandomTranscriptionTask

--- controllers/transcription-tasks/getRandomTranscriptionTask.js
+++ controllers/transcription-tasks/getRandomTranscriptionTask.js
@@ -1,25 +1,26 @@
 const { TranscriptionTask: Task } = require("../../models");
 const { NotFound } = require("http-errors");
+const characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const getRandomTranscriptionTask = async (req, res, next) => {
 	const { numberofsymbols } = req.query;
 
+	const query = characters[Math.floor(Math.random() * characters.length)]
 	const querySt = {
-		eng: { $exists: true },
+		eng: { $regex: `^${query}` },
 		$and: [
-			{ $expr: { $gt: [{ $strLenCP: '$eng' }, Number(numberofsymbols)- 1] } },
+			{ $expr: { $gt: [{ $strLenCP: '$eng' }, Number(numberofsymbols) - 1] } },
 			{ $expr: { $lt: [{ $strLenCP: '$eng' }, Number(numberofsymbols) + 1] } }
 		],
 	}
 
 	const tasks = await Task.find(querySt)
-	// .limit(2000)
 	if (!tasks.length) {
-		throw new NotFound("No data by your query");
+		throw new NotFound("No data");
 	}
 
 	const task = tasks[Math.floor(Math.random() * tasks.length)]
-	
+
 	res.status(200).json({
 		task,
 	});
