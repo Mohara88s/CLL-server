9d728c847e5c1fc3ce70fc3100165adf590aaf3c
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2023-12-05 11:13:02 +0200

    fixed getRandomTranscriptionTask

--- controllers/transcription-tasks/getRandomTranscriptionTask.js
+++ controllers/transcription-tasks/getRandomTranscriptionTask.js
@@ -1,25 +1,25 @@
 const { TranscriptionTask: Task } = require("../../models");
 const { NotFound } = require("http-errors");
-const characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const getRandomTranscriptionTask = async (req, res, next) => {
 	const { numberofsymbols } = req.query;
 
-	const query = characters[Math.floor(Math.random() * characters.length)]
 	const querySt = {
-		eng: { $regex: `^${query}` },
 		$and: [
 			{ $expr: { $gt: [{ $strLenCP: '$eng' }, Number(numberofsymbols) - 1] } },
-			{ $expr: { $lt: [{ $strLenCP: '$eng' }, Number(numberofsymbols) + 1] } }
+			{ $expr: { $lt: [{ $strLenCP: '$eng' }, Number(numberofsymbols) + 1] } },
+			{ $expr: { $gt: [{ $strLenCP: '$utrn' }, Number(numberofsymbols) - 1] } },
+			{ $expr: { $lt: [{ $strLenCP: '$utrn' }, Number(numberofsymbols) + 1] } }
 		],
 	}
-
 	const tasks = await Task.find(querySt)
 	if (!tasks.length) {
 		throw new NotFound("No data");
 	}
 
-	const task = tasks[Math.floor(Math.random() * tasks.length)]
+	const filtredTasks = tasks.filter(task=>!task.eng.includes(" "))
+
+	const task = filtredTasks[Math.floor(Math.random() * filtredTasks.length)]
 
 	res.status(200).json({
 		task,
